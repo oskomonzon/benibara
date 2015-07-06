a2aec88ca05e11ebe4fee2c6c1d0ca445c9d5450
OskO <oskomonzon@yahoo.com.ar>
2015-07-06 15:43:02 -0300

    Font-Size & .js fix

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -24,7 +24,7 @@ $(window).scroll(function(){
 
 // Smooth Scroll
 $('a[href*=#]:not([href=#])').click(function () {
-  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+  if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') || location.hostname === this.hostname) {
     var target = $(this.hash);
     target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
     if (target.length) {
